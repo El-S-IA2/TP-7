f27189fe5c3795b21718f9186169099dbe46838a
Elarays <56716561+Elarays@users.noreply.github.com>
2019-11-27 15:11:24 +0100

    Update Codeur.java

--- src/Codeur.java
+++ src/Codeur.java
@@ -12,19 +12,34 @@ public class Codeur {
 
     public static void main(String[] args) throws IOException {
 
+       if (args.length!=4)
+    		erreur("4 arguments requis");
+    	
+       int cle=-1;
+       try{
+        	cle = Integer.parseInt(args[0]);
+        }
+        catch (NumberFormatException e) {
+        	erreur ("args[0] doit etre un entier");
+        }
+       
+       if (cle<1 | cle>25) erreur("clé invalide");
+       
         int cle = Integer.parseInt(args[0]);
         String type = args[1];
         Cesar cesar = new Cesar(cle);
         String line;
-
-        BufferedReader lecteur = new BufferedReader(new FileReader(args[2]));
+         
+       File file=new File(args[2]);
+        if(!file.exists()) erreur ("fichier introuvable");
+        if(!file.isFile()) erreur ("fichier invalide");
+        BufferedReader lecteur = new BufferedReader(new FileReader(file));
 
         FileWriter e = new FileWriter(args[3]);
         BufferedWriter ecriture = new BufferedWriter((e));
 
 
-        if (args[1] == "code")
-        {
+            if (args[1].equals("code"))        {
             while ((line = lecteur.readLine()) != null)
             {
                 ecriture.write(cesar.code(line));
@@ -33,8 +48,7 @@ public class Codeur {
             ecriture.close();
             lecteur.close();
         }
-        else
-            {
+            else if(args[1].equals("decode"))            {
                 while ((line = lecteur.readLine()) != null)
                 {
                 ecriture.write(cesar.deCode(line));
@@ -43,6 +57,12 @@ public class Codeur {
                 ecriture.close();
                 lecteur.close();
             }
+       else erreur("type de procedure invalide");
+    }
+   
+     static void erreur(String msg) {
+    	System.err.println(msg);
+    	System.exit(1);
     }
 }
 
